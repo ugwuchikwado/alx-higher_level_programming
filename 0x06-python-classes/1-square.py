ae68d90f0f65295035d8c65996384f656dbffee5
= <ugwuchikwadoaugustine@gmail.com>
2023-12-20 11:24:15 +0100

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 
+A square class
+
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """__init__
+
+        The __init__ method initializes the size value
+        of the square.
+
+        """
+
