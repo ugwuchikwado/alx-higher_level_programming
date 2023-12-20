abd419a52310bf6040ccdd7a69505faf85ef75dd
= <ugwuchikwadoaugustine@gmail.com>
2023-12-20 11:57:11 +0100

    python classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+class Square:
+    """Square Class
+
+    A Square Class
+
+    """
+
+    def __init__(self, size=0):
+
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
