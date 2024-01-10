5ade3706c8c4f212708e5885802f7d9453b943a2
= <ugwuchikwadoaugustine@gmail.com>
2024-01-10 14:19:09 +0100

    python inheritance

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module that inherits from BaseGeometry (7-base_geometry.py).
+(task based on 8-rectangle.py)that inherits from BaseGeometry
+(7-base_geometry.py). (task based on 8-rectangle.py)
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class rectangle that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, 'height', height)
+        self.__height = height
+        BaseGeometry.integer_validator(self, 'width', width)
+        self.__width = width
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
