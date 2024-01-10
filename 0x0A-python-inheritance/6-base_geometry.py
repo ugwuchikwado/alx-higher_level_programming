e2d617c3217c6c8fb1904e6f4d579627366aba46
= <ugwuchikwadoaugustine@gmail.com>
2024-01-10 13:57:34 +0100

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry (based on 5-base_geometry.py)
+"""
+
+
+class BaseGeometry:
+    """ empty class"""
+    def area(self):
+        """
+        raises an 'area() not implemented exception'
+        """
+        raise Exception('area() is not implemented')
