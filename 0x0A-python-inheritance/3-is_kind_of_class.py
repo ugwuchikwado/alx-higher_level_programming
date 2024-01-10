4cfcf83ba37663c96a7a2b97a6d03401354672eb
= <ugwuchikwadoaugustine@gmail.com>
2024-01-10 13:39:19 +0100

    python inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that checks object is an instance of, or if the object
+is an instance of a class that inherited from, the specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns True if the object is an instance of, or if the object
+    is an instance
+    of a class that inherited from, the specified class ; otherwise False
+    """
+    return isinstance(obj, a_class)
